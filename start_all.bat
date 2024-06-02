884069325f9f0c4859864de65a209ac08eee6a81
TinyShellzz <tinyshellzz@gmail.com>
2024-06-02 12:56:36 +0800

    fix

--- start_all.bat
+++ start_all.bat
@@ -1,3 +1,6 @@
 title start_all
+cd "Lagrange.OneBot"
+start /min "" "Lagrange.OneBot.exe" -i
+cd ..
 start /min "" cmd /k call "start_kiki_bot.bat" -i
 start /min "" cmd /k call "start_kiki_backend.bat" -i
--- start_kiki_bot.bat
+++ start_kiki_bot.bat
@@ -1,11 +1,8 @@
+@echo off
 title kiki_bot
 Set  "anaconda_dir=E:\DevInstall\Anaconda"
 
-cd "Lagrange.OneBot"
-start /min "" "Lagrange.OneBot.exe" -i
-cd ..
-call %anaconda_dir%/Scripts/activate.bat %anaconda_dir%
-REM "切换conda虚拟环境。 如果你有自己的虚拟环境, 需要自行更改"
+call conda init
 call conda activate mc
 cd kiki_bot
 nb run
