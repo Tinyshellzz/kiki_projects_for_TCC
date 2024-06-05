65ca25a6210791fd947bd539c1bca2194715ff45
TinyShellzz <tinyshellzz@gmail.com>
2024-06-05 10:26:28 +0800

    online对接qq

--- kiki_bot/kiki_bot/plugins/nonebot-plugin-kiki/kiki_message_hub.py
+++ kiki_bot/kiki_bot/plugins/nonebot-plugin-kiki/kiki_message_hub.py
@@ -30,6 +30,7 @@ match_rules = [
     ['^/whitelist add .*$', whitelist.add, 0],              # 添加玩家到白名单
     ['^/whitelist insert .*$', whitelist.insert, 0],        # 将玩家插入数据库
     ['^(找人|search).*$', whitelist.get, 0],               # 万能查询
+    ['^online$', whitelist.online, 0],                         # 查看在线晚间
     ['^.*' + code_prefix + '[0-9a-zA-Z]{6}.*$', whitelist.code, 10],   # 白名单验证码
     ['(怎么进服|服务器地址|怎么玩)', replies.ip, 10],
     # ['(未知主机|连接超时|dns|连不上|连接失败|连不了)', replies.dns, 10],
